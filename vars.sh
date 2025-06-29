9b5d00f91023721b2c58fc7aa7e2adcc0e03a2aa
Tamal267 <syedtamal@gmail.com>
2025-06-29 21:43:55 +0600

    talenthuntiso

--- vars.sh
+++ vars.sh
@@ -6,14 +6,10 @@ DOTS_DIR="dots"
 SCRIPTS_DIR="scripts"
 ADMIN_SCRIPTS_DIR="postinst/admin/"
 
-ADMIN_USER="admin"
-ADMIN_PASSWORD='$1$qOaJpMTn$udSLvrlWWiT43He1M2wy5.'
+ADMIN_USER="admin_pc"
+ADMIN_PASSWORD='$1$HvrO9a.3$xiiVtUtpPMOPWgiYamq/k1'
 # encrypted with $(openssl passwd -1 PLAINTEXTPASSWORD)
 
-TEAM_USER="contestant"
-TEAM_PASSWORD='$1$92hYmWFk$qPeMP85ThFuzlKbdgh3cu0'
+TEAM_USER="mcc"
+TEAM_PASSWORD='$1$KwqD27gq$xADGuJWA0m6TKAcZqXPU6.'
 # encrypted with $(openssl passwd -1 PLAINTEXTPASSWORD)
-
-MOCK_USER="mock"
-MOCK_PASSWORD='$1$MzrwRNfh$km8LUveoj/rZZDZiEA8PB1'
-# encrypted with $(openssl passwd -1 'PLAINTEXTPASSWORD')
