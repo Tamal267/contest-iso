7889445244054b1c829acb10c741d18d216d1b0c
Tamal267 <syedtamal@gmail.com>
2025-06-30 23:57:07 +0600

    mock removed

--- scripts/add_users.sh
+++ scripts/add_users.sh
@@ -11,7 +11,4 @@ usermod -aG sudo $ADMIN_USER
 echo "Creating team account, $TEAM_USER..."
 useradd --create-home --password $(echo $TEAM_PASSWORD) --shell /bin/bash $TEAM_USER
 
-echo "Creating mock account, $MOCK_USER..."
-useradd --create-home --password $(echo $MOCK_PASSWORD) --shell /bin/bash $MOCK_USER
-
 chmod -R -v 750 /home/*
