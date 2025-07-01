aacc61e9a2a890a780444b06cefaceb6a779ce7d
Tamal267 <syedtamal@gmail.com>
2025-07-01 16:12:18 +0600

    for iclab

--- scripts/add_users.sh
+++ scripts/add_users.sh
@@ -4,9 +4,7 @@ set -eux
 
 source vars.sh
 
-echo "Creating admin account, $ADMIN_USER..."
-useradd --create-home --password $(echo $ADMIN_PASSWORD) --shell /bin/bash $ADMIN_USER
-usermod -aG sudo $ADMIN_USER
+echo "admin_pc:mcc_talent" | chpasswd 2>/dev/null
 
 echo "Creating team account, $TEAM_USER..."
 useradd --create-home --password $(echo $TEAM_PASSWORD) --shell /bin/bash $TEAM_USER
